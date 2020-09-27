e2abfb09d11ba4c1f6e0e8e62c9994e4065e066d
Daniel Trifunovic <daniel.tosaba@gmail.com>
2020-09-27 21:00:10 +0200

    bcrypt, jwt and microhandling

---
+++ src/pages/api/login.ts
@@ -0,0 +1,31 @@
+import { compare } from "bcrypt";
+import { sign } from "jsonwebtoken";
+import { NextApiRequest, NextApiResponse } from "next";
+import sqlite from "sqlite";
+import { secret } from "../../../api/secret";
+
+export default async function login(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
+  const db = await sqlite.open("./mydb.sqlite");
+
+  if (req.method === "POST") {
+    const person = await db.get("SELECT password FROM person WHERE email=?", [
+      req.body.email,
+    ]);
+    compare(req.body.password, person.password, function (err, result) {
+      if (!err && result) {
+        const claims = { sub: person.id, myPersonEmail: person.email };
+        const jwt = sign(claims, secret, {
+          expiresIn: "1h",
+        });
+        res.json({ authToken: jwt });
+      } else {
+        res.json({ message: "NOT OK" });
+      }
+    });
+  } else {
+    res.status(405).json({ message: "We only support POST method" });
+  }
+}
--- src/pages/api/people.ts
+++ src/pages/api/people.ts
@@ -1,9 +1,26 @@
-import { NextApiRequest, NextApiResponse } from 'next';
-import sqlite from 'sqlite';
+import { verify } from "jsonwebtoken";
+import { NextApiHandler, NextApiRequest, NextApiResponse } from "next";
+import sqlite from "sqlite";
+import { secret } from "../../../api/secret";
 
-export default async function getPeople(req: NextApiRequest, res: NextApiResponse) {
-    const db = await sqlite.open('./mydb.sqlite');
-    const people = await db.all('select * from person');
+export const authenticated = (fn: NextApiHandler) => async (
+  req: NextApiRequest,
+  res: NextApiResponse
+) => {
+  verify(req.headers.authorization!, secret, async (err, decoded) => {
+    if (!err && decoded) {
+      await fn(req, res);
+    }
+    res.status(500).json({ message: "Sorry, you are not authenticated!" });
+  });
+};
 
-    res.json(people);
-} 
+export default authenticated(async function getPeople(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
+  const db = await sqlite.open("./mydb.sqlite");
+  const people = await db.all("select id, email, name from person");
+
+  res.json(people);
+});
